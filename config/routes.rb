6b0f6d856e58e6151cd5ddbab55ac6a36a441f2d
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-06-23 15:13:52 +0900

    Add redux todolist sample app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'react/index'
+
   namespace :api do
     resources :todos, except: [:new, :edit]
 
@@ -6,4 +8,6 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'react#index'
 end
